5a96199cab119b7429bb76dc428f102d21e209a5
Lance Lot <phucphuong1293@gmail.com>
2017-12-19 13:41:13 +0700

    Mod: thêm datalist cho input box.

--- static/js/tab1.js
+++ static/js/tab1.js
@@ -1,11 +1,18 @@
+var list_option = '';
+
 function rendDefaultSamples() {
     var $area = $(document.getElementById('samples'));
     $.getJSON("/samples/1.json", function (json) {
 
         var regex = json.regex;
         var input = document.getElementById('input-regex-tab1');
+
         input.value = regex;
         input.style.color = 'green';
+
+        json.values.forEach(function (sample) {
+            list_option += '<option value="' + sample.value + '">';
+        });
 
         json.values.forEach(function (sample) {
             var d = new Date();
@@ -13,19 +20,47 @@ function rendDefaultSamples() {
             $area.append('<div id="' + time + '" style="display: none; margin-top: 20px;"></div>');
             var $new = $('#' + time);
             var color;
-            if (sample.answer === 1){
+            if (sample.answer === 1) {
                 color = "greenyellow";
-            }else {
+            } else {
                 color = "crimson";
             }
 
+            // $new.append('<div class="row">' +
+            //     '<div class="col-3"></div>' +
+            //     '<div class="col-3"><input class="input-sample-tab1" type="text" oninput="check()" placeholder="Enter your sample here..." value="'+ sample.value +'"></div>' +
+            //     '<div class="col-1"><div class="matching-state" style="background-color: ' + color + '"></div></div>' +
+            //     '<div class="col-2"><button class="btn-remove-sample" onClick="removeLine(event)">Remove</button></div>' +
+            //     '</div>');
+            // $new.fadeIn(1000);
+
             $new.append('<div class="row">' +
                 '<div class="col-3"></div>' +
-                '<div class="col-3"><input class="input-sample-tab1" type="text" oninput="check()" placeholder="Enter your sample here..." value="'+ sample.value +'"></div>' +
+                '<div class="col-3">' +
+                '<form action="#">' +
+                '<input list="browsers" class="input-sample-tab1" type="text" oninput="check()" placeholder="Enter your sample here..." value="' + sample.value + '">' +
+                '<datalist id="browsers">\n' +
+                list_option +
+                '</datalist>' +
+                '</form>' +
+                '</div>' +
                 '<div class="col-1"><div class="matching-state" style="background-color: ' + color + '"></div></div>' +
                 '<div class="col-2"><button class="btn-remove-sample" onClick="removeLine(event)">Remove</button></div>' +
                 '</div>');
             $new.fadeIn(1000);
+
+            // $new.append('<form action="/action_page.php" method="get">\n' +
+            //             '  <input list="browsers" name="browser">\n' +
+            //             '  <datalist id="browsers">\n' +
+            //             '    <option value="Internet Explorer">\n' +
+            //             '    <option value="Firefox">\n' +
+            //             '    <option value="Chrome">\n' +
+            //             '    <option value="Opera">\n' +
+            //             '    <option value="Safari">\n' +
+            //             '  </datalist>\n' +
+            //             '  <input type="submit">\n' +
+            //             '</form>');
+            // $new.fadeIn(1000);
         }); // this will show the info it in firebug console
     });
 }
@@ -40,9 +75,24 @@ function addNewLine() {
     var time = d.getTime();
     $area.append('<div id="' + time + '" style="display: none; margin-top: 20px;"></div>');
     var $new = $('#' + time);
+    // $new.append('<div class="row">' +
+    //     '<div class="col-3"></div>' +
+    //     '<div class="col-3"><input class="input-sample-tab1" type="text" oninput="check()" placeholder="Enter your sample here..."></div>' +
+    //     '<div class="col-1"><div class="matching-state"></div></div>' +
+    //     '<div class="col-2"><button class="btn-remove-sample" onClick="removeLine(event)">Remove</button></div>' +
+    //     '</div>');
+    // $new.fadeIn(1000);
+
     $new.append('<div class="row">' +
         '<div class="col-3"></div>' +
-        '<div class="col-3"><input class="input-sample-tab1" type="text" oninput="check()" placeholder="Enter your sample here..."></div>' +
+        '<div class="col-3">' +
+        '<form action="#">' +
+        '<input list="browsers" class="input-sample-tab1" type="text" oninput="check()" placeholder="Enter your sample here...">' +
+        '<datalist id="browsers">\n' +
+        list_option +
+        '</datalist>' +
+        '</form>' +
+        '</div>' +
         '<div class="col-1"><div class="matching-state"></div></div>' +
         '<div class="col-2"><button class="btn-remove-sample" onClick="removeLine(event)">Remove</button></div>' +
         '</div>');
@@ -78,7 +128,7 @@ function check() {
         var samples = document.getElementsByClassName('input-sample-tab1');
         for (var i = 0; i < samples.length; i++) {
             var str = (samples[i].value);
-            var state = samples[i].parentNode.parentNode.getElementsByClassName('matching-state')[0];
+            var state = samples[i].parentNode.parentNode.parentNode.getElementsByClassName('matching-state')[0];
             if (str.length > 0 && input.length > 0) {
                 var regex = /\/(.*)\/(.*)$/;
                 var found = input.match(regex);
--- static/js/tab2.js
+++ static/js/tab2.js
@@ -1,10 +1,17 @@
+var list_option_tab2 = '';
+
 function rendDefaultSamplesTab2() {
     var $area = $(document.getElementById('samples-2'));
     $.getJSON("/samples/2.json", function (json) {
         var regex = json.regex;
-        var input = document.getElementById('input-regex-tab1');
+        var input = document.getElementById('input-regex-tab2');
+        document.getElementById('input-for-replacing').value = json.replace;
         input.value = regex;
         input.style.color = 'green';
+
+        json.values.forEach(function (sample) {
+            list_option_tab2 += '<option value="' + sample.value + '"></option>';
+        });
 
         json.values.forEach(function (sample) {
             var d = new Date();
@@ -20,14 +27,17 @@ function rendDefaultSamplesTab2() {
 
             $new.append('<div class="row">' +
                 '<div class="col-1"></div>' +
-                '<div class="col-3"><input class="input-sample-tab2" type="text" oninput="check_tab2()" placeholder="Enter your sample here..." value="'+ sample.regex + '"></div>' +
-                '<div class="col-3"><input class="result-sample-tab2" type="text" disabled placeholder="Result of replacing" value="' + sample.answer + '" style="background-color: '+ color +'"></div>' +
-                '<div class="col-3"><input class="ref-sample-tab2" type="text" oninput="check_tab2()" placeholder="Reference" value="' + sample.expectation +  '"></div>' +
+                '<div class="col-3"><form><input list="option2" class="input-sample-tab2" type="text" oninput="check_tab2()" placeholder="Enter your sample here..." value="' + sample.value + '">' +
+                '<datalist id="option2">' +
+                list_option_tab2 +
+                '</datalist></form></div>' +
+                '<div class="col-3"><input class="result-sample-tab2" type="text" disabled placeholder="Result of replacing" value="' + sample.answer + '" style="border: '+ color +' solid 3px' + '"></div>' +
+                '<div class="col-3"><input class="ref-sample-tab2" type="text" oninput="check_tab2()" placeholder="Reference" value="' + sample.expectation + '"></div>' +
                 '<div class="col-1"><button class="btn-remove-sample" onClick="removeSample(event)">Remove</button></div>' +
                 '</div>');
             $new.fadeIn(1000);
 
-        }); // this will show the info it in firebug console
+        });
     });
 }
 
@@ -43,13 +53,15 @@ function addNewSample() {
     var $new = $('#' + time);
     $new.append('<div class="row">' +
         '<div class="col-1"></div>' +
-        '<div class="col-3"><input class="input-sample-tab2" type="text" oninput="check_tab2()" placeholder="Enter your sample here..."></div>' +
+        '<div class="col-3"><form><input list="option2" class="input-sample-tab2" type="text" oninput="check_tab2()" placeholder="Enter your sample here...">' +
+        '<datalist id="option2">' +
+        list_option_tab2 +
+        '</datalist></form></div>' +
         '<div class="col-3"><input class="result-sample-tab2" type="text" disabled placeholder="Result of replacing"></div>' +
         '<div class="col-3"><input class="ref-sample-tab2" type="text" oninput="check_tab2()" placeholder="Reference"></div>' +
         '<div class="col-1"><button class="btn-remove-sample" onClick="removeSample(event)">Remove</button></div>' +
         '</div>');
     $new.fadeIn(1000);
-
 }
 
 function removeSample(event) {
@@ -75,14 +87,14 @@ function removeLastSample(event) {
 function check_tab2() {
     var input_regex = document.getElementById('input-regex-tab2').value;
     var replacing_input = document.getElementById('input-for-replacing').value;
-
     if (checkRegExp(input_regex)) {
         document.getElementById('input-regex-tab2').style.color = "green";
         var samples = document.getElementsByClassName('input-sample-tab2');
         for (var i = 0; i < samples.length; i++) {
             var input = (samples[i].value);
-            var result_input_box = samples[i].parentNode.parentNode.getElementsByClassName('result-sample-tab2')[0];
-            var ref_text = samples[i].parentNode.parentNode.getElementsByClassName('ref-sample-tab2')[0].value;
+            var result_input_box = samples[i].parentNode.parentNode.parentNode.getElementsByClassName('result-sample-tab2')[0];
+            var ref_input_box = samples[i].parentNode.parentNode.parentNode.getElementsByClassName('ref-sample-tab2')[0];
+            var ref_text = ref_input_box.value;
             if (input.length > 0 && input_regex.length > 0) {
                 var regex = /\/(.*)\/(.*)$/;
                 var found = input_regex.match(regex);
@@ -93,13 +105,15 @@ function check_tab2() {
                 var pattern = new RegExp('^' + result_text + '$');
                 var match = pattern.test(ref_text);
                 if (match) {
-                    result_input_box.style.backgroundColor = 'greenyellow';
+                    result_input_box.style.border = 'greenyellow solid 3px';
                 } else {
-                    result_input_box.style.backgroundColor = 'red';
+                    result_input_box.style.border = 'red solid 3px';
                 }
 
             } else if (input.length === 0) {
                 result_input_box.value = '';
+                ref_input_box.value = '';
+                result_input_box.style.border = '#4274ff solid 3px';
             }
         }
     }
